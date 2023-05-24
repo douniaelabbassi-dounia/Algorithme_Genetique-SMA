8ea8fb275664627b9b711f8bb00a1a41ff6bf4c1
DOUNIA EL ABBASSI <73197564+douniaelabbassi-dounia@users.noreply.github.com>
2023-05-24 17:08:09 +0100

    Update Individual.java

--- src/ma/enset/ga/sequencial/Individual.java
+++ src/ma/enset/ga/sequencial/Individual.java
@@ -3,23 +3,23 @@ package ma.enset.ga.sequencial;
 import java.util.Random;
 
 public class Individual implements Comparable{
-    //Bonjour
-    //chromosome
     private char genes[]=new char[7];
-    private int fitness;
-    public Individual() {
-        Random rnd=new Random();
-        for (int i=0;i<genes.length;i++){
-            genes[i]=  (char) (rnd.nextInt(26) + 'A');
+    private String alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        }
-    }
+    String target = "bonjour";
+    private int fitness;
+   public Individual() {
+       Random rnd=new Random();
+       for (int i=0;i<genes.length;i++){
+           int index= rnd.nextInt(alphabet.length());
+           genes[i]=alphabet.charAt(index);
+       }
+   }
     public void calculateFitness(){
-        String target = "BONJOUR";
         fitness=0;
-        for (int i = 0; i < genes.length; i++) {
-            if (genes[i] == target.charAt(i)) {
-                fitness++;
+        for (int i=0;i<genes.length;i++){
+            if(target.charAt(i)==genes[i]){
+                fitness=fitness+1;
             }
         }
     }
